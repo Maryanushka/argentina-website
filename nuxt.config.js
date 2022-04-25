ef6f5381ca9d885728198557c37918f8a10d09d9
Maryanushka <marya.vadymovna@gmail.com>
2022-04-25 18:51:55 +0200

    icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -126,7 +126,7 @@ export default {
 
 	fontawesome: {
 		icons: {
-			solid: ['faAngleRight', 'faPhone', 'faEnvelope', 'faLongArrowAltRight', 'faArrowRight', 'faArrowLeft', 'faThumbsUp', 'faLock', 'faUsers', 'faBars', 'faTimes', 'faChevronRight', 'faEllipsisV', 'faPlayCircle', 'faPlay', 'faHandHoldingMedical', 'faPassport', 'faSmile', 'faCoins', 'faHeart', 'faGlobeAmericas', 'faSun', 'faMapMarkerAlt'],
+			solid: ['faAngleRight', 'faPhone', 'faEnvelope', 'faLongArrowAltRight', 'faArrowRight', 'faArrowLeft', 'faThumbsUp', 'faLock', 'faUsers', 'faBars', 'faTimes', 'faChevronRight', 'faEllipsisV', 'faPlayCircle', 'faPlay', 'faHandHoldingMedical', 'faPassport', 'faSmile', 'faCoins', 'faHeart', 'faGlobeAmericas', 'faSun', 'faMapMarkerAlt', 'faBaby', 'faUserShield'],
 			regular: ['faClock', 'faEnvelope', 'faPlayCircle'],
 			brands: ['faFacebook', 'faInstagram', 'faYoutube', 'faGratipay', 'faTelegram', 'faWhatsapp'],
 		},
